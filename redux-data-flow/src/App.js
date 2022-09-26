ce696eda60f893686afac93083f0d2606bede7d8
arifpehlivan <arifpehlivanapap@gmail.com>
2022-09-26 10:28:33 +0300

    add data flow reducer

--- redux-data-flow/src/App.js
+++ redux-data-flow/src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
+import PostsList from "./features/posts/PostsList";
+import AddPostForm from './features/posts/AddPostForm';
 
 function App() {
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <main>
+      <AddPostForm/>
+      <PostsList/>
+    </main>
   );
 }
 
---
+++ redux-data-flow/src/features/posts/postsSlice.js
@@ -0,0 +1,36 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = [
+    {
+        id: "1", title: "Redux Toolkit", content: "Redux"
+    },
+    {
+        id: "2", title: "Redux", content: "Redux Toolkit"
+    },
+]
+
+const postsSlice = createSlice({
+    name: "posts",
+    initialState,
+    reducers: {
+        postAdded:{
+            reducer(state,action){
+            state.push(action.payload)
+        },
+        prepare(title,content,userId){
+            return{
+                payload:{
+                    id: nanoid(),
+                    title,
+                    content,
+                    userId
+                }
+            }
+        }
+    }
+}})
+
+export const selectAllPosts = (state) => state.posts;
+export const {postAdded} = postsSlice.actions;
+
+export default postsSlice.reducer
